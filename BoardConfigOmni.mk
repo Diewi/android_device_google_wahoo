f708b01c48549ae5f76503728eb8dbbf75f88d00
Joe Maples <joe@frap129.org>
2017-11-08 17:44:47 -0500

    wahoo: Add inline kernel building support
    Change-Id: I642ce2baddd14efde19e8a4d0a89a1c03c3d055b
    Signed-off-by: Joe Maples <joe@frap129.org>

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -15,3 +15,10 @@
 #
 # Contains the wahoo and omni specific makefile definitions.
 #
+
+# Inline kernel building
+TARGET_KERNEL_SOURCE := kernel/google/wahoo
+BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_NEEDS_DTBOIMAGE := true
+TARGET_KERNEL_ARCH := arm64
