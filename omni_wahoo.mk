fc64295cbd828498167853e83f07575fd288b652
Christopher N. Hesse <raymanfx@gmail.com>
2018-01-23 23:42:55 +0100

    Enable EUICC Sim
    Change-Id: If43e859c80e69b35b08c1097ad268cf4411d2d8b

--- omni_wahoo.mk
+++ omni_wahoo.mk
@@ -29,3 +29,7 @@ BOARD_SEPOLICY_DIRS += \
 
 # Omni config
 $(call inherit-product, vendor/omni/config/common.mk)
+
+# EUICC feature
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.euicc.xml
